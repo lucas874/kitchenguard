6adfc3222f22b447e70c5ffbeb7748fd9572fa54
lucas874 <60431606+lucas874@users.noreply.github.com>
2021-05-24 10:23:30 +0200

    Update SPMain.py

--- Python/SPMain.py
+++ Python/SPMain.py
@@ -6,12 +6,14 @@ from SPModel import SPModel, SPZigbeeDevice
 
 if __name__ == "__main__":
     devices_model = SPModel()
-    devices_model.add([SPZigbeeDevice(id_ = "LED1", type_ = "led"),  # Add devices to model
-                       SPZigbeeDevice(id_ = "LED2", type_ = "led"),
+    devices_model.add([SPZigbeeDevice(id_ = "LED2", type_ = "led"),  # Add devices to model
+                       SPZigbeeDevice(id_ = "LED3", type_ = "led"),
+                       SPZigbeeDevice(id_ = "LED4", type_ = "led"),
                        SPZigbeeDevice(id_ = "NEO", type_ = "power plug"),
-                       SPZigbeeDevice(id_ = "PIR1", type_ = "pir", LED_friend="LED1"), # Associate PIR with LED
+                       SPZigbeeDevice(id_ = "PIR1", type_ = "pir"), # Associate PIR with LED 
                        SPZigbeeDevice(id_ = "PIR2", type_ = "pir", LED_friend="LED2"),
-                       SPZigbeeDevice(id_ = "PIR3", type_ = "pir")])
+                       SPZigbeeDevice(id_ = "PIR3", type_ = "pir", LED_friend="LED3"), 
+                       SPZigbeeDevice(id_ = "PIR4", type_ = "pir", LED_friend="LED4")])
 
     # Create a controller and give it the data model that was instantiated.
     controller = SPController(devices_model)
